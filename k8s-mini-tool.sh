5764922d79f64b3c16084cb30662ce892d0cee70
k.kovalevskii <k.kovalevskii@brandquad.ru>
2022-05-20 00:48:12 +0300

    add files upload/download support from/in Pod, add save config

--- k8s-mini-tool.sh
+++ k8s-mini-tool.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-ALLOW_FILE_LOAD=0
+ALLOW_FILE_LOAD=1
 USE_COMMAND2=0
 CERT=ca-auto.pem
 EDITOR=nano
 CONTAINER=app
-FILEPATH=app
 COMMAND="(bash || ash || sh)"
 COMMAND2=""
+CONFIGFILE="k8s-mini-tool.set"
 
 f_sep(){
 echo "---------------------------"
@@ -22,7 +22,7 @@ echo -e "\033[1;36m$1\033[0m"
 }
 
 f_warning_text(){
-echo -e "\033[31m$1\033[0m"
+echo -e "\033[31mWARNING:$1\033[0m"
 }
 
 f_get_contexts(){
@@ -32,43 +32,17 @@ f_sep
 }
 
 f_run_command(){
-if [ $ALLOW_FILE_LOAD = 1 ]
-then
-    f_text "Input filename to copy in pod(50kB max):";read FILENAME
-    f_write_file_to_pod
-fi
 f_text "Run command $COMMAND2 $COMMAND in $NAMESPACE $POD $CONTAINER"
 if [ "$USE_COMMAND2" = 1 ] && [ "$COMMAND2" != "" ]
 then
+    echo -n > $CONFIGFILE
+    echo "export NAMESPACE=$NAMESPACE" >> $CONFIGFILE
+    echo "export POD=$POD" >> $CONFIGFILE
+    echo "export CONTAINER=$CONTAINER" >> $CONFIGFILE
     kubectl exec -i -t -n $NAMESPACE $POD -c $CONTAINER "--" sh -c "$COMMAND2;$COMMAND"
 else
     kubectl exec -i -t -n $NAMESPACE $POD -c $CONTAINER "--" sh -c "$COMMAND"
 fi
-}
-
-f_write_file_to_pod() {
-    if [ $FILENAME ] && [ -e $FILENAME ]
-    then
-        FILESIZE=$(wc -c $FILENAME | awk '{print $1}')
-        if [ $FILESIZE -lt 51200 ]
-        then
-            f_text "kubectl cp $FILENAME $NAMESPACE/$POD:/$FILEPATH/$FILENAME -c $CONTAINER"
-            while true; do
-                read -p "Do you want to send $FILENAME? (y/n)" yn
-                case $yn in
-                    [Yy]* )
-                    kubectl cp $FILENAME $NAMESPACE/$POD:/$FILEPATH/$FILENAME -c $CONTAINER
-                    break;;
-                    [Nn]* )
-                    echo "$FILENAME file was not sent"
-                    break;;
-                    * ) echo "Please answer y (yes) or n (no)";;
-                esac
-            done
-        fi
-    else
-        f_warning_text "No file to send or max limit(50kb) reached"
-    fi
 }
 
 f_get_deploy_status(){
@@ -124,23 +98,25 @@ kubectl get ns
 }
 
 f_menu_switch_context(){
-f_text_bl "Switch k8s context";f_sep
+f_text_bl "Switch k8s Context";f_sep
 f_get_contexts
 f_text "Input NAME context to switch to:";read CONTEXTNAME
 kubectl config use-context $CONTEXTNAME
 f_get_contexts
+echo -n > $CONFIGFILE
 }
 
 f_menu_delete_context(){
-f_text_bl "Delete k8s context";f_sep
+f_text_bl "Delete k8s Context";f_sep
 f_get_contexts
 f_text "Input NAME context to be deleted:";read CONTEXTNAME
 kubectl config unset contexts.$CONTEXTNAME
 f_get_contexts
+echo -n > $CONFIGFILE
 }
 
 f_menu_get_pod(){
-f_text_bl "Get pod and run command";f_sep
+f_text_bl "Get New Pod and Run Command";f_sep
 f_get_contexts
 kubectl get ns
 while true; do
@@ -159,8 +135,103 @@ done
 f_run_command
 }
 
+f_menu_get_selected_pod(){
+f_text_bl "Get Selected Pod and Run Command";f_sep
+if [ $(stat -c "%s" $CONFIGFILE) -eq 0 ]
+then
+    f_warning_text "No Pod Selected. Select one firstly"
+else
+f_run_command
+fi
+}
+
+f_menu_get_files_from_pod() {
+while true; do
+    f_text_bl "Get Files from Pod";f_sep
+    f_text "Input full path to file IN POD:";read FILEFROMPOD
+    f_text "Input full path to LOCAL file, which will be save FROM POD:";read FILELOCAL
+    f_text "EXEC:kubectl cp $NAMESPACE/$POD:$FILEFROMPOD $FILELOCAL -c $CONTAINER"
+    read -p "Do you want to recieve file from pod using current settings? (y/n)" yn
+    case $yn in
+        [Yy]* )
+        kubectl cp $NAMESPACE/$POD:$FILEFROMPOD $FILELOCAL -c $CONTAINER
+        if [ $FILELOCAL ]
+        then
+            f_text "Check File: File recived succesfully"
+        else
+            f_warning_text "File doesnt recieve"
+        fi
+        ;;
+        [Nn]* )
+        f_text "Input full path to file IN POD:";read FILEFROMPOD
+        f_text "Input full path to LOCAL file, which will be save FROM POD:";read FILELOCAL
+        f_text "Input NAMESPACE:";read NAMESPACE
+        f_text "Input POD:";read POD
+        f_text "Input CONTAINER:";read CONTAINER
+        f_text "EXEC:kubectl cp $NAMESPACE/$POD:$FILEFROMPOD $FILELOCAL -c $CONTAINER"
+        kubectl cp $NAMESPACE/$POD:$FILEFROMPOD $FILELOCAL -c $CONTAINER
+        if [ $FILELOCAL ]
+        then
+            f_text "Check File: File recived succesfully"
+        else
+            f_warning_text "File doesnt recieve"
+        fi
+        ;;
+        * ) echo "Please answer y (yes) or n (no).";;
+   esac
+done
+}
+
+f_menu_send_files_to_pod() {
+f_text_bl "Sending Files to Pod";f_sep
+if [ $ALLOW_FILE_LOAD = 1 ]
+then
+    while true; do
+        f_text "Input full path to LOCAL file, which will be send in pod(50kB max):";read FILELOCAL
+        if [ $FILELOCAL ] && [ -e $FILELOCAL ]
+        then
+            FILESIZE=$(wc -c $FILELOCAL | awk '{print $1}')
+            if [ $FILESIZE -lt 51200 ]
+            then
+                f_text "Input full path to send file IN POD:";read FILETOPOD
+                f_text "EXEC:kubectl cp $FILELOCAL $NAMESPACE/$POD:$FILETOPOD -c $CONTAINER"
+                read -p "Do you want to SEND file in pod using current settings? (y/n)" yn
+                case $yn in
+                    [Yy]* )
+                    kubectl cp $FILELOCAL $NAMESPACE/$POD:$FILETOPOD -c $CONTAINER
+                    f_text "Check file:";kubectl exec -i -t -n $NAMESPACE $POD -c $CONTAINER "--" sh -c "ls -l $FILETOPOD;exit"
+                    ;;
+                    [Nn]* )
+                    f_text "Input full path to LOCAL file, which will be send in pod(50kB max):";read FILELOCAL
+                    f_text "Input full path to send file IN POD:";read FILETOPOD
+                    f_text "Input NAMESPACE:";read NAMESPACE
+                    f_text "Input POD:";read POD
+                    f_text "Input CONTAINER:";read CONTAINER
+                    f_text "EXEC:kubectl cp $FILELOCAL $NAMESPACE/$POD:$FILETOPOD -c $CONTAINER"
+                    read -p "Do you want to SEND file in pod using current settings? (y/n)" yn
+                    case $yn in
+                        [Yy]* )
+                        kubectl cp $FILELOCAL $NAMESPACE/$POD:$FILETOPOD -c $CONTAINER
+                        f_text "Check file:";kubectl exec -i -t -n $NAMESPACE $POD -c $CONTAINER "--" sh -c "ls -l $FILETOPOD;exit"
+                        ;;
+                        [Nn]* )
+                        f_warning_text "Aborted"
+                        ;;
+                        * ) echo "Please answer y (yes) or n (no).";;
+                    esac
+                esac
+            fi
+        else
+        f_warning_text "No file to send or max limit(50kb) reached"
+        fi
+    done
+else
+    f_warning_text "Sending files is disabled"
+fi
+}
+
 f_menu_edit_deploy(){
-f_text_bl "Edit Deploy file";f_sep
+f_text_bl "Edit Deploy File";f_sep
 f_get_contexts
 kubectl get ns
 while true; do
@@ -180,7 +251,7 @@ while true; do
         read -p "Do you want to edit $DEP? (y/n)" yn
         case $yn in
             [Yy]* )
-            f_text "$EDITOR kubectl edit deployment/$DEP --namespace $NAMESPACE"
+            f_text "EXEC:$EDITOR kubectl edit deployment/$DEP --namespace $NAMESPACE"
             KUBE_EDITOR=$EDITOR kubectl edit deployment/$DEP --namespace $NAMESPACE
             f_get_deploy_status
             break;;
@@ -199,11 +270,27 @@ f_menu_choice(){
 /\ /_/ _\   / / // / //   /  /_//_// 
                                      "
     f_sep
+    f_text_bl "Selected Context:"
+    kubectl config current-context
+    if [ $(stat -c "%s" $CONFIGFILE) -eq 0 ]
+    then
+        f_text_bl "No current Objects Selected"
+    else
+        f_text_bl "Selected Objects:"
+        source $CONFIGFILE
+        echo "NAMESPACE=$NAMESPACE"
+        echo "POD=$POD"
+        echo "CONTAINER=$CONTAINER"
+    fi
+    f_sep
     echo "1 - Setup New k8s Access"
     echo "2 - Delete Context"
     echo "3 - Switch Context"
-    echo "4 - Get Pod and Run Command"
-    echo "5 - Edit Deploy File"
+    echo "4 - Get New Pod and Run Command"
+    echo "5 - Get Selected Pod and Run Command"
+    echo "6 - Edit Deploy File"
+    echo "7 - Get Files from Pod"
+    echo "8 - Send Files to Pod"
     f_sep
     echo "Enter your choice:";read choice;f_sep
         case $choice in
@@ -216,10 +303,18 @@ f_menu_choice(){
             "4")
             f_menu_get_pod;;
             "5")
+            f_menu_get_selected_pod;;
+            "6")
             f_menu_edit_deploy;;
+            "7")
+            f_menu_get_files_from_pod;;
+            "8")
+            f_menu_send_files_to_pod;;
+            "exit")
+            exit;;
             "777")
-            f_text_bl "v1.4";;
-            * ) echo "Please enter valid number or hit CTRL+C for exit.";;
+            f_text_bl "v1.5";;
+            * ) echo "Please enter valid number or input "exit" for exit.";;
         esac
     done
 }
